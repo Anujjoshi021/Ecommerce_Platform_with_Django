3659cbd3fae2d0ab555ae7700e8c2dc1e3a56d63
Anujjoshi021 <anujjoshi595@gmail.com>
2024-01-23 21:24:26 +0530

    Add Project files to the repo

---
+++ ecommerce/ecommerceapp/keys.py
@@ -0,0 +1,5 @@
+MID=""
+MK=""
+
+# here these two things, merchant ID and key both should be hidden from others. So i am not typing it here and for these two things you first have to activate your account on paytm developer setting then generate an API key for testing purpose.
+
